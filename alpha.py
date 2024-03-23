3e7e25498716292d7fb89ced681366b8561f9b3c
TechAdix <126282292+TechAdrianPro@users.noreply.github.com>
2024-03-23 16:30:19 +0100

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,11 +1,12 @@
 from pytube import YouTube
+import os
 
-def download_video(url):
+def download_video(url, save_path):
     try:
         yt = YouTube(url, on_progress_callback=progress_function)
         stream = yt.streams.get_highest_resolution()
         print("Rozpoczynam pobieranie...")
-        stream.download()
+        stream.download(output_path=save_path)
         print("Pobieranie zakończone pomyślnie!")
     except Exception as e:
         print(f"Błąd: {str(e)}")
@@ -19,7 +20,10 @@ def progress_function(stream, chunk, bytes_remaining):
 def main():
     # URL filmu YouTube
     url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"  # Możesz zmienić ten URL na dowolny inny
-    download_video(url)
+    save_path = "./videos"  # Ścieżka, w której chcesz zapisać plik
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    download_video(url, save_path)
 
 if __name__ == "__main__":
     main()
